f8ff71e20fa266f603ea450b4faf36133ff4da27
Brandon <pacewicbrandon@gmail.com>
2021-11-14 10:36:04 -0800

    basic web test working

--- sourceCode/__init__.py
+++ sourceCode/__init__.py
@@ -4,4 +4,10 @@ def create_boa():
     boa = Flask(__name__)
     boa.config['SECRET_KEY'] = 'ihaveboainmyroom'
 
+    from .views import views
+    from .auth import auth
+
+    boa.register_blueprint(views, url_prefix = '/')
+    boa.register_blueprint(auth, url_prefix = '/')
+
     return boa
