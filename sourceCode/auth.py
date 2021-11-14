bcf42a9ab02cfed46d8b628d118a1c988c862ea5
Brandon <pacewicbrandon@gmail.com>
2021-11-14 11:09:36 -0800

    basic directory test

--- sourceCode/auth.py
+++ sourceCode/auth.py
@@ -2,3 +2,14 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/login')
+def login():
+    return '<h1>Login</h1>'
+
+@auth.route('/logout')
+def logout():
+    return '<h1>Logout</h1>'
+
+@auth.route('/register')
+def register():
+    return '<h1>Register</h1>'
