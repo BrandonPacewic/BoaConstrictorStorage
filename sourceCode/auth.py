7c24a8879075f6731c70f26a5d949fc8653435b7
Brandon <pacewicbrandon@gmail.com>
2021-11-14 14:13:21 -0800

    data save for account creation

--- sourceCode/auth.py
+++ sourceCode/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
@@ -10,6 +10,14 @@ def login():
 def logout():
     return '<h1>Logout</h1>'
 
-@auth.route('/register')
+@auth.route('/register', methods = ['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        boa_inital = request.form.get('boa_inital')
+        boa_color = request.form.get('boa_color')
+        boa_size = request.form.get('boa_size')
+        boa_sequence1 =  request.form.get('boa_sequence1')
+        boa_sequence2 = request.form.get('boa_sequence2')
+
+
     return render_template("register.html")
