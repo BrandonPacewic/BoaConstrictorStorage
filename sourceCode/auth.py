6413cba2ceaa893be3824b8c3be4f755b85bd073
Brandon <pacewicbrandon@gmail.com>
2021-11-14 15:37:30 -0800

    message flashing

--- sourceCode/auth.py
+++ sourceCode/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -18,6 +18,22 @@ def register():
         boa_size = request.form.get('boa_size')
         boa_sequence1 =  request.form.get('boa_sequence1')
         boa_sequence2 = request.form.get('boa_sequence2')
+
+
+        if len(boa_inital) < 3:
+            flash('Boa_name must be longer than 2 characters.', category = 'badBoa')
+        elif len(boa_color) < 2:
+            flash('Boa_color must be longer than 1 character.', category = 'badBoa')
+        elif len(boa_size) < 4:
+            flash('Boa_size must be longer than 3 characters.', category = 'badBoa')
+        elif len(boa_sequence1) < 7:
+            flash('Boa_sequence must be longer than 6 characters.', category = 'badBoa')
+        elif boa_sequence1 != boa_sequence2:
+            flash('Boa_sequences do not match.', category = 'badBoa')
+        else:
+            # create boa
+            flash('Boa Created Sucessfully!', category = 'boaGood')
+
 
 
     return render_template("register.html")
